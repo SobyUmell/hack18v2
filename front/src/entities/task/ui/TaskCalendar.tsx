c3344b24ff7199cdbdd1d5945bb8bb893e74b9a5
Sobar <sdruza4@gmail.com>
2024-11-23 07:56:48 +0300

    feat: Task stuff

---
+++ front/src/entities/task/ui/TaskCalendar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const TaskCalendar = (props: Props) => {
+  return <div></div>;
+};
+
+export default TaskCalendar;
--- front/src/entities/task/ui/TaskEdit.tsx
+++ front/src/entities/task/ui/TaskEdit.tsx
@@ -1,9 +1,30 @@
 import React from "react";
-
+import { TextField } from "@mui/material";
+import s from "./TaskEdit.module.scss";
+import TaskCalendar from "./TaskCalendar";
 type Props = {};
 
 const TaskEdit = (props: Props) => {
-  return <div></div>;
+  return (
+    <div className={s.task_edit}>
+      <div className={s.title}>
+        <TextField
+          id="standard-basic"
+          label="Название задачи"
+          variant="standard"
+        />
+      </div>
+      <div className={s.description}>
+        <TextField
+          id="standard-basic"
+          label="Описание задачи"
+          multiline
+          maxRows={4}
+        />
+      </div>
+      <TaskCalendar />
+    </div>
+  );
 };
 
 export default TaskEdit;
